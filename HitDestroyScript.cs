f115920edeeb5fd522e286fee1a31490d7344e37
Santhosh-N <59107079+Santhosh-N@users.noreply.github.com>
2021-01-31 03:11:39 +0000

    Add files via upload

---
+++ HitDestroyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDestroyScript : MonoBehaviour
+{
+    public GameObject DestroyEffect;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Dash")
+        {
+            Destroy(gameObject);
+            Instantiate(DestroyEffect, transform.position, Quaternion.identity);
+        }
+    }
+}
